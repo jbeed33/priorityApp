125a8054cb5a3f44c9be40eca112c9c73629c8a3
Joshua Beed <jbeed33@gmail.com>
2023-11-17 23:00:05 -0800

    updated auth route not tested nor complete

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,23 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const createToken = async (id, name) => {
-  return jwt.sign({ id, name }, process.env.TOKEN_KEY);
+  return await jwt.sign({ id, name }, process.env.TOKEN_KEY);
+};
+
+const authorize = async (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).json({ message: "Authorization token is missing" });
+  }
+
+  await jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    req.user = decoded;
+    next();
+  });
 };
 
 const post_signup_user = async (req, res, next) => {
@@ -24,22 +40,28 @@ const post_signup_user = async (req, res, next) => {
     const newUser = new User({ userId, name, email, username, password: hash });
     await newUser.save();
     const createdToken = await createToken(newUser._id, name);
-    res.cookie("auth", await createdToken, { httpOnly: true });
+    res.cookie("auth", createdToken, { httpOnly: true });
     res.send({ msg: "Account created." });
   } catch (e) {
+    // still need to do a better job at error handling.
     console.error(`something went wrong in user sign up ${e}`);
   }
 };
 
 const post_login_user = (req, res, next) => {
+  //Do stuff
+  // create token
+  //else error handle
   res.send({ msg: "Login called" });
 };
 
 const post_sign_out_user = (req, res, next) => {
+  // set cookie to empty and has a very short expiration date
   res.send({ msg: "Sign out called" });
 };
 
 const patch_edit_user = (req, res, next) => {
+  //This comes later
   res.send({ msg: "Edit called" });
 };
 
@@ -48,4 +70,5 @@ module.exports = {
   post_sign_out_user,
   post_signup_user,
   patch_edit_user,
+  authorize,
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 router.post("/signup", authController.post_signup_user);
-router.post("/login", authController.post_login_user);
+router.post("/login", authController.authorize, authController.post_login_user);
 router.post("/signout", authController.post_sign_out_user);
 
 router.patch("/edit", authController.patch_edit_user);
