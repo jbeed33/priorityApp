bf155dda4bf10467aa4f2779062706f9f3b65984
Joshua Beed <jbeed33@gmail.com>
2023-12-29 15:25:59 -0800

    Finished styling for Task list card

--- frontend/src/components/overview/Overview.js
+++ frontend/src/components/overview/Overview.js
@@ -3,13 +3,13 @@ import Card from "../card/card";
 import "./overview.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import ScheduleCard from "../scheduleCard/ScheduleCard";
 
 export default function Overview() {
   return (
     <>
       <div id="overview-container">
         <div class="overview-cols cols-bg">
-          {/* <OverviewFilter></OverviewFilter> */}
           <h1>Active Tasks</h1>
           <div id="filter-btn">
             <h2>Filter</h2>
@@ -33,10 +33,10 @@ export default function Overview() {
           </div>
         </div>
         <div class="overview-cols cols-sm">
-          <h1>This week</h1>
-          <div>
-            <Card></Card>
-            <Card></Card>
+          <h1>Due this week</h1>
+          <div id="overview-task-list-container">
+            <ScheduleCard></ScheduleCard>
+            <ScheduleCard></ScheduleCard>
           </div>
         </div>
         <div class="overview-cols cols-sm">
---
+++ frontend/src/components/scheduleCard/ScheduleCard.js
@@ -0,0 +1,24 @@
+import "./scheduleCard.css";
+
+export default function ScheduleCard() {
+  return (
+    <>
+      <div id="card-container">
+        <div id="card-header">
+          <h1>Dececmber 13th</h1>
+        </div>
+        <ul id="card-list">
+          <li>
+            <h3>Go to store</h3>
+          </li>
+          <li>
+            <h3>Finish exams for today</h3>
+          </li>
+          <li>
+            <h3>Eat more vegetables even if you do not like them.</h3>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
