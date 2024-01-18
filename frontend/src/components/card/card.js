ff402d8f4ad9623844da03cc0c2284b63d0dff3e
Joshua Beed <jbeed33@gmail.com>
2024-01-18 11:55:44 -0800

    updated cards so that the user can now set a task to compelete

--- frontend/src/components/card/card.js
+++ frontend/src/components/card/card.js
@@ -12,6 +12,28 @@ export default function Card(props) {
     });
     props.setDisplayCard(true);
   }
+
+  async function completeTask(e) {
+    e.stopPropagation();
+    const updatedSegment = { status: 1, taskId: props.id };
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedSegment),
+      credentials: "same-origin",
+    };
+
+    try {
+      let data = await fetch("/api/task/edit", options);
+      let res = await data.json();
+      console.log("Response", res);
+    } catch (e) {
+      console.error("Something went wrong: ", e);
+    }
+  }
+
   console.log(props);
   return (
     <>
@@ -20,7 +42,11 @@ export default function Card(props) {
           <div id="card-title">{props.title}</div>
           <div id="card-options">
             <button>
-              <FontAwesomeIcon icon={faCheck} class="card-icon" />
+              <FontAwesomeIcon
+                onClick={(e) => completeTask(e)}
+                icon={faCheck}
+                class="card-icon"
+              />
             </button>
             <button>
               <FontAwesomeIcon icon={faEllipsis} class="card-icon" />
