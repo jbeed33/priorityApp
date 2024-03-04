41ae18de91440c7ecfc27cfdec72e1ba9e6af3c2
Joshua Beed <jbeed33@gmail.com>
2024-03-04 09:26:53 -0800

    updated age for session to two days

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 3010;
-const THIRTYMINUTES = 1000 * 60 * 30;
+const TWODAYS = 86400000 * 2;
 
 //need this to override CORS POLICY
 app.use(cors());
@@ -24,7 +24,7 @@ app.use(
     secret: "top secret",
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: THIRTYMINUTES },
+    cookie: { maxAge: TWODAYS },
   })
 );
 
